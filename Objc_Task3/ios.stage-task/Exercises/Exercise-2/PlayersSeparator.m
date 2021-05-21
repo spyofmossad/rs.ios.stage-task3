7d5ee039483aec59bd867324f3d0c8c0d809f333
Dzmitry Navitski <spyofmossad@gmail.com>
2021-05-21 16:49:54 +0300

    feat: add exercise2 implementation

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,57 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+        
+    if ([ratingArray count] < 3) {
+        return 0;
+    }
+    
+    NSMutableArray *increaseBuffer;
+    NSMutableArray *decreaseBuffer;
+    NSMutableSet *finalSet = [NSMutableSet new];
+    
+    for (int i = 0; i < [ratingArray count] - 2; i++) {
+        
+        increaseBuffer = [[NSMutableArray alloc] initWithObjects:ratingArray[i], nil];
+        decreaseBuffer = [[NSMutableArray alloc] initWithObjects:ratingArray[i], nil];
+        
+        for (int j = i+1; j < [ratingArray count]; j++) {
+            if ([increaseBuffer count] > 1) {
+                [increaseBuffer removeLastObject];
+            }
+            
+            if ([decreaseBuffer count] > 1) {
+                [decreaseBuffer removeLastObject];
+            }
+            
+            if ([ratingArray[j] intValue] > [[increaseBuffer lastObject] intValue]) {
+                [increaseBuffer addObject:[ratingArray objectAtIndex:j]];
+            } else if ([[ratingArray objectAtIndex:j] intValue] < [[decreaseBuffer lastObject] intValue]) {
+                [decreaseBuffer addObject:[ratingArray objectAtIndex:j]];
+            }
+            
+            for (int k = j+1; k < [ratingArray count]; k++) {
+                if ([ratingArray[k] intValue] > [[increaseBuffer lastObject] intValue]) {
+                    [increaseBuffer addObject:[ratingArray objectAtIndex:k]];
+                } else if ([[ratingArray objectAtIndex:k] intValue] < [[decreaseBuffer lastObject] intValue]) {
+                    [decreaseBuffer addObject:[ratingArray objectAtIndex:k]];
+                }
+                
+                if ([increaseBuffer count] > 2) {
+                    [finalSet addObject: [[increaseBuffer valueForKey:@"description"] componentsJoinedByString:@""]];
+                    [increaseBuffer removeLastObject];
+                }
+                
+                if ([decreaseBuffer count] > 2) {
+                    [finalSet addObject: [[decreaseBuffer valueForKey:@"description"] componentsJoinedByString:@""]];
+                    [decreaseBuffer removeLastObject];
+                }
+                
+            }
+            
+        }
+    }
+    return [finalSet count];
 }
 
 @end
